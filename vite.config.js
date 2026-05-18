cd08c9a697494a9566746fb3b70705bfe28008d7
Quang New <nbxchiquang@gmail.com>
2026-05-19 00:40:44 +0700

    Add GitHub Actions workflow for deploying to gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+const githubPagesBase =
+  process.env.GITHUB_ACTIONS === 'true' && repositoryName ? `/${repositoryName}/` : '/'
+
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH ?? githubPagesBase,
   plugins: [vue()],
 })
